225fab306620e5d5a22110c237ef5ad2185c47bb
maulayaaa020-cloud <mauiszotunkhasanah@gmail.com>
2026-05-16 09:45:14 +0700

    chore: minify nocopy.js dan otp.js

--- nocopy.js
+++ nocopy.js
@@ -1,7 +1,1 @@
-(function () {
-    document.addEventListener('contextmenu', function (e) { e.preventDefault(); });
-    document.addEventListener('copy',        function (e) { e.preventDefault(); });
-    document.addEventListener('cut',         function (e) { e.preventDefault(); });
-    document.addEventListener('selectstart', function (e) { e.preventDefault(); });
-    document.addEventListener('dragstart',   function (e) { e.preventDefault(); });
-})();
+(function(){document.addEventListener('contextmenu',function(e){e.preventDefault()});document.addEventListener('copy',function(e){e.preventDefault()});document.addEventListener('cut',function(e){e.preventDefault()});document.addEventListener('selectstart',function(e){e.preventDefault()});document.addEventListener('dragstart',function(e){e.preventDefault()})})();
